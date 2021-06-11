84c26cb815e35c6a19401ee9cbc2aeefcafa4251
jiye-7 <jy522hi@gmail.com>
2021-06-11 17:38:02 +0900

    navbar

--- client/src/Components/navbar/navbar.jsx
+++ client/src/Components/navbar/navbar.jsx
@@ -2,7 +2,26 @@ import React, { Component } from 'react';
 
 class Navbar extends Component {
   render() {
-    return <div>Helo</div>;
+    return (
+      <div className="navigation-container">
+        <nav>
+          <ul>
+            <li>PlayList</li>
+            <li>My List</li>
+            <li>
+              <img
+                className="user-img"
+                src={
+                  this.props.userInfo
+                    ? `https://blogfiles.pstatic.net/MjAxNzA1MjNfMTA1/MDAxNDk1NTQxMjY2ODY0.0pCWaDaIhTLs3TlZAgnx73ossilphBYxgzAWnc577bgg.-bEfv7csnuZUGtq5-NmplDjYYXEWvzWNdvklQE9sE8Qg.PNG.yej1686/Untitled-1-01.png`
+                    : ``
+                }
+              ></img>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
   }
 }
 
