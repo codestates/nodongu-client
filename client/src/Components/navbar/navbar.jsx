8cfe0b962c5e81ac65a0744e4ffb0b6e561abcfe
jiye-7 <jy522hi@gmail.com>
2021-06-14 16:19:50 +0900

    Create popup

--- client/src/Components/navbar/navbar.jsx
+++ client/src/Components/navbar/navbar.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
+import Popup from '../popup/popup';
 import './navbar.css';
 import logo from '../../Utils/images/logo_3.png';
 import quokka from '../../Utils/images/quokka.jpg';
@@ -9,31 +10,31 @@ class Navbar extends Component {
     fakeData: {
       userId: 1,
       nickname: 'jiye-7',
-      email: 'jy522hi@gmail.com',
+      email: 'jyhi@gmail.com',
       image: '',
     },
   };
 
   render() {
     return (
-      <div className='navigation-container'>
-        <nav className='navigation-container-nav'>
-          <ul className='nav__ul'>
-            <li className='li_logo'>
+      <div className="navigation-container">
+        <nav className="navigation-container-nav">
+          <ul className="nav__ul">
+            <li className="li_logo">
               <img src={logo} style={({ width: 55 }, { height: 55 })} />
             </li>
-            <li className='li_playlist'>PlayList</li>
-            <li className='li_mylist'>My List</li>
+            <li className="li_playlist">PlayList</li>
+            <li className="li_mylist">My List</li>
           </ul>
           <img
-            className='user-img'
+            className="user-img user-img-small"
             src={
               this.state.fakeData.image == false
                 ? `${quokka}`
                 : `${this.state.fakeData.image}`
             }
-            style={({ width: 60 }, { height: 60 })}
           ></img>
+          <Popup />
         </nav>
       </div>
     );
