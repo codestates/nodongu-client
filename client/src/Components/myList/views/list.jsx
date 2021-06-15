7549ae94bed2cd7b35421241172a591339b4a4dc
wldns0622 <wldns12378@gmail.com>
2021-06-16 04:34:48 +0900

    Add style to list

--- client/src/Components/myList/views/list.jsx
+++ client/src/Components/myList/views/list.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 import quokka from '../../../Utils/images/quokka.jpg';
 
 function List(props) {
+  const handleListClick = (event) => {
+    if (event.target.classList[0] !== 'playlist') {
+      // 해당 리스트의 뮤직리스트 요청
+      // 뮤직리스트 app의 상태 업로드
+      // 페이지 이동
+    }
+  };
+
   const [image1, image2, image3, image4] = props.playlist.thumbnails;
   return (
     <div>
-      <div className='playlist'>
+      <div className='playlist' onClick={handleListClick}>
         <div className='playlist__image'>
           <img src={image1 ? image1 : quokka} alt='' />
           <img src={image2 ? image2 : quokka} alt='' />
