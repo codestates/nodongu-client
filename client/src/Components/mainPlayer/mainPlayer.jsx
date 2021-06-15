6beb9ba0acee517253dfab4ad1f2fb22891bdfb8
jiye-7 <jy522hi@gmail.com>
2021-06-15 16:10:34 +0900

    Create Main Player Layout

---
+++ client/src/Components/mainPlayer/mainPlayer.jsx
@@ -0,0 +1,334 @@
+import React, { Component } from 'react';
+import './mainPlayer.css';
+import albumImage from '../../Utils/images/album-image.png';
+
+class MainPlayer extends Component {
+  render() {
+    return (
+      <main className="main-player-container">
+        <div className="main-play">
+          <div className="play-img">
+            <img src={albumImage} className="play-album-img" />
+          </div>
+          <div className="main-play-list">
+            <p className="next-track">다음 트랙</p>
+            <ul className="play-list-ul">
+              <li className="play-list-li">
+                <div className="play-list-left">
+                  {/* <div className="play-list-song"> */}
+                  <img src={albumImage} className="play-list-song-img" />
+                  {/* </div> */}
+                  <div className="play-list-content">
+                    <div className="play-list-content-title">Butter</div>
+                    <div className="play-list-content-singer">
+                      방탄소년단(BTS)
+                    </div>
+                  </div>
+                </div>
+                <div className="play-list-right">
+                  <div className="play-replay-time">2:45</div>
+                </div>
+              </li>
+              <li className="play-list-li">
+                <div className="play-list-left">
+                  <img src={albumImage} className="play-list-song-img" />
+                  <div className="play-list-content">
+                    <div className="play-list-content-title">Butter</div>
+                    <div className="play-list-content-singer">
+                      방탄소년단(BTS)
+                    </div>
+                  </div>
+                </div>
+                <div className="play-list-right">
+                  <div className="play-replay-time">2:45</div>
+                </div>
+              </li>
+              <li className="play-list-li">
+                <div className="play-list-left">
+                  <img src={albumImage} className="play-list-song-img" />
+                  <div className="play-list-content">
+                    <div className="play-list-content-title">Butter</div>
+                    <div className="play-list-content-singer">
+                      방탄소년단(BTS)
+                    </div>
+                  </div>
+                </div>
+                <div className="play-list-right">
+                  <div className="play-replay-time">2:45</div>
+                </div>
+              </li>
+              <li className="play-list-li">
+                <div className="play-list-left">
+                  <img src={albumImage} className="play-list-song-img" />
+                  <div className="play-list-content">
+                    <div className="play-list-content-title">Butter</div>
+                    <div className="play-list-content-singer">
+                      방탄소년단(BTS)
+                    </div>
+                  </div>
+                </div>
+                <div className="play-list-right">
+                  <div className="play-replay-time">2:45</div>
+                </div>
+              </li>
+              <li className="play-list-li">
+                <div className="play-list-left">
+                  <img src={albumImage} className="play-list-song-img" />
+                  <div className="play-list-content">
+                    <div className="play-list-content-title">Butter</div>
+                    <div className="play-list-content-singer">
+                      방탄소년단(BTS)
+                    </div>
+                  </div>
+                </div>
+                <div className="play-list-right">
+                  <div className="play-replay-time">2:45</div>
+                </div>
+              </li>
+              <li className="play-list-li">
+                <div className="play-list-left">
+                  <img src={albumImage} className="play-list-song-img" />
+                  <div className="play-list-content">
+                    <div className="play-list-content-title">Butter</div>
+                    <div className="play-list-content-singer">
+                      방탄소년단(BTS)
+                    </div>
+                  </div>
+                </div>
+                <div className="play-list-right">
+                  <div className="play-replay-time">2:45</div>
+                </div>
+              </li>
+              <li className="play-list-li">
+                <div className="play-list-left">
+                  <img src={albumImage} className="play-list-song-img" />
+                  <div className="play-list-content">
+                    <div className="play-list-content-title">Butter</div>
+                    <div className="play-list-content-singer">
+                      방탄소년단(BTS)
+                    </div>
+                  </div>
+                </div>
+                <div className="play-list-right">
+                  <div className="play-replay-time">2:45</div>
+                </div>
+              </li>
+              <li className="play-list-li">
+                <div className="play-list-left">
+                  <img src={albumImage} className="play-list-song-img" />
+                  <div className="play-list-content">
+                    <div className="play-list-content-title">Butter</div>
+                    <div className="play-list-content-singer">
+                      방탄소년단(BTS)
+                    </div>
+                  </div>
+                </div>
+                <div className="play-list-right">
+                  <div className="play-replay-time">2:45</div>
+                </div>
+              </li>
+              <li className="play-list-li">
+                <div className="play-list-left">
+                  <img src={albumImage} className="play-list-song-img" />
+                  <div className="play-list-content">
+                    <div className="play-list-content-title">Butter</div>
+                    <div className="play-list-content-singer">
+                      방탄소년단(BTS)
+                    </div>
+                  </div>
+                </div>
+                <div className="play-list-right">
+                  <div className="play-replay-time">2:45</div>
+                </div>
+              </li>
+              <li className="play-list-li">
+                <div className="play-list-left">
+                  <img src={albumImage} className="play-list-song-img" />
+                  <div className="play-list-content">
+                    <div className="play-list-content-title">Butter</div>
+                    <div className="play-list-content-singer">
+                      방탄소년단(BTS)
+                    </div>
+                  </div>
+                </div>
+                <div className="play-list-right">
+                  <div className="play-replay-time">2:45</div>
+                </div>
+              </li>
+              <li className="play-list-li">
+                <div className="play-list-left">
+                  <img src={albumImage} className="play-list-song-img" />
+                  <div className="play-list-content">
+                    <div className="play-list-content-title">Butter</div>
+                    <div className="play-list-content-singer">
+                      방탄소년단(BTS)
+                    </div>
+                  </div>
+                </div>
+                <div className="play-list-right">
+                  <div className="play-replay-time">2:45</div>
+                </div>
+              </li>
+              <li className="play-list-li">
+                <div className="play-list-left">
+                  <img src={albumImage} className="play-list-song-img" />
+                  <div className="play-list-content">
+                    <div className="play-list-content-title">Butter</div>
+                    <div className="play-list-content-singer">
+                      방탄소년단(BTS)
+                    </div>
+                  </div>
+                </div>
+                <div className="play-list-right">
+                  <div className="play-replay-time">2:45</div>
+                </div>
+              </li>
+              <li className="play-list-li">
+                <div className="play-list-left">
+                  <img src={albumImage} className="play-list-song-img" />
+                  <div className="play-list-content">
+                    <div className="play-list-content-title">Butter</div>
+                    <div className="play-list-content-singer">
+                      방탄소년단(BTS)
+                    </div>
+                  </div>
+                </div>
+                <div className="play-list-right">
+                  <div className="play-replay-time">2:45</div>
+                </div>
+              </li>
+              <li className="play-list-li">
+                <div className="play-list-left">
+                  <img src={albumImage} className="play-list-song-img" />
+                  <div className="play-list-content">
+                    <div className="play-list-content-title">Butter</div>
+                    <div className="play-list-content-singer">
+                      방탄소년단(BTS)
+                    </div>
+                  </div>
+                </div>
+                <div className="play-list-right">
+                  <div className="play-replay-time">2:45</div>
+                </div>
+              </li>
+              <li className="play-list-li">
+                <div className="play-list-left">
+                  <img src={albumImage} className="play-list-song-img" />
+                  <div className="play-list-content">
+                    <div className="play-list-content-title">Butter</div>
+                    <div className="play-list-content-singer">
+                      방탄소년단(BTS)
+                    </div>
+                  </div>
+                </div>
+                <div className="play-list-right">
+                  <div className="play-replay-time">2:45</div>
+                </div>
+              </li>
+              <li className="play-list-li">
+                <div className="play-list-left">
+                  <img src={albumImage} className="play-list-song-img" />
+                  <div className="play-list-content">
+                    <div className="play-list-content-title">Butter</div>
+                    <div className="play-list-content-singer">
+                      방탄소년단(BTS)
+                    </div>
+                  </div>
+                </div>
+                <div className="play-list-right">
+                  <div className="play-replay-time">2:45</div>
+                </div>
+              </li>
+              <li className="play-list-li">
+                <div className="play-list-left">
+                  <img src={albumImage} className="play-list-song-img" />
+                  <div className="play-list-content">
+                    <div className="play-list-content-title">Butter</div>
+                    <div className="play-list-content-singer">
+                      방탄소년단(BTS)
+                    </div>
+                  </div>
+                </div>
+                <div className="play-list-right">
+                  <div className="play-replay-time">2:45</div>
+                </div>
+              </li>
+              <li className="play-list-li">
+                <div className="play-list-left">
+                  <img src={albumImage} className="play-list-song-img" />
+                  <div className="play-list-content">
+                    <div className="play-list-content-title">Butter</div>
+                    <div className="play-list-content-singer">
+                      방탄소년단(BTS)
+                    </div>
+                  </div>
+                </div>
+                <div className="play-list-right">
+                  <div className="play-replay-time">2:45</div>
+                </div>
+              </li>
+              <li className="play-list-li">
+                <div className="play-list-left">
+                  <img src={albumImage} className="play-list-song-img" />
+                  <div className="play-list-content">
+                    <div className="play-list-content-title">Butter</div>
+                    <div className="play-list-content-singer">
+                      방탄소년단(BTS)
+                    </div>
+                  </div>
+                </div>
+                <div className="play-list-right">
+                  <div className="play-replay-time">2:45</div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="play-set-up">
+          <div className="play-range">
+            <button className="pre-music-btn">
+              <i className="fas fa-step-backward"></i>
+            </button>
+            <button className="pause-music-btn">
+              <i className="fas fa-pause"></i>
+            </button>
+            <button className="next-music-btn">
+              <i className="fas fa-step-forward"></i>
+            </button>
+            <span className="music-time">2:34/3:37</span>
+          </div>
+          <div className="play-song-info">
+            <div className="play-song-info-content">
+              <img src={albumImage} className="song-info-img" />
+              <div className="song-info-content">
+                <div className="song-info-title">Butter</div>
+                <div className="song-info-singer">
+                  방탄소년단(BTS) • Butter(Hotter, Sweeter, Cooler) • 2021
+                </div>
+              </div>
+              <div className="song-add">
+                <button className="song-add-btn">
+                  <i className="fas fa-plus"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="play-song-setting">
+            <button>
+              <i className="fas fa-volume-up"></i>
+            </button>
+            <button>
+              <img className="fas fa-redo" />
+            </button>
+            <button>
+              <i className="fas fa-random"></i>
+            </button>
+          </div>
+        </div>
+      </main>
+    );
+  }
+}
+
+export default MainPlayer;
