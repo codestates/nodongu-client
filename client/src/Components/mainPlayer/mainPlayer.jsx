e8cfc9358b60b96019b9b7604fdf77db926e0a40
jiye-7 <jy522hi@gmail.com>
2021-06-16 19:05:06 +0900

    signup state update

--- client/src/Components/mainPlayer/mainPlayer.jsx
+++ client/src/Components/mainPlayer/mainPlayer.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './mainPlayer.css';
 import albumImage from '../../Utils/images/album-image.png';
 import AddPlayer from '../addPlayer/addPlayer';
+import YouTube from 'react-youtube';
 
 class MainPlayer extends Component {
   state = {
@@ -20,142 +21,157 @@ class MainPlayer extends Component {
     });
   };
 
+  // _onReady(event) {
+  //   // access to player in all event handlers via event.target
+  //   event.target.pauseVideo();
+  // }
+
   render() {
+    // player
+    // const options = {
+    //   width: '300',
+    //   height: '50',
+    //   playerVars: {
+    //     autoplay: 1,
+    //     origin: 'http://localhost:3000',
+    //   },
+    // };
+
     return (
-      <main className='main-player-container'>
-        <div className='main-play'>
-          <div className='play-img'>
-            <img src={albumImage} className='play-album-img' alt='album' />
+      <main className="main-player-container">
+        <div className="main-play">
+          <div className="play-img">
+            <img src={albumImage} className="play-album-img" alt="album" />
           </div>
-          <div className='main-play-list'>
-            <p className='next-track'>다음 트랙</p>
-            <ul className='play-list-ul'>
-              <li className='play-list-li'>
-                <div className='play-list-left'>
+          <div className="main-play-list">
+            <p className="next-track">다음 트랙</p>
+            <ul className="play-list-ul">
+              <li className="play-list-li">
+                <div className="play-list-left">
                   {/* <div className="play-list-song"> */}
                   <img
                     src={albumImage}
-                    className='play-list-song-img'
-                    alt='album'
+                    className="play-list-song-img"
+                    alt="album"
                   />
                   {/* </div> */}
-                  <div className='play-list-content'>
-                    <div className='play-list-content-title'>Butter</div>
-                    <div className='play-list-content-singer'>
+                  <div className="play-list-content">
+                    <div className="play-list-content-title">Butter</div>
+                    <div className="play-list-content-singer">
                       {/* 방탄소년단(BTS) */}
                     </div>
                   </div>
                 </div>
-                <div className='play-list-right'>
-                  <div className='play-replay-time'>2:45</div>
+                <div className="play-list-right">
+                  <div className="play-replay-time">2:45</div>
                 </div>
               </li>
-              <li className='play-list-li'>
-                <div className='play-list-left'>
+              <li className="play-list-li">
+                <div className="play-list-left">
                   <img
                     src={albumImage}
-                    className='play-list-song-img'
-                    alt='album'
+                    className="play-list-song-img"
+                    alt="album"
                   />
-                  <div className='play-list-content'>
-                    <div className='play-list-content-title'>Butter</div>
-                    <div className='play-list-content-singer'>
+                  <div className="play-list-content">
+                    <div className="play-list-content-title">Butter</div>
+                    <div className="play-list-content-singer">
                       {/* 방탄소년단(BTS) */}
                     </div>
                   </div>
                 </div>
-                <div className='play-list-right'>
-                  <div className='play-replay-time'>2:45</div>
+                <div className="play-list-right">
+                  <div className="play-replay-time">2:45</div>
                 </div>
               </li>
-              <li className='play-list-li'>
-                <div className='play-list-left'>
+              <li className="play-list-li">
+                <div className="play-list-left">
                   <img
                     src={albumImage}
-                    className='play-list-song-img'
-                    alt='album'
+                    className="play-list-song-img"
+                    alt="album"
                   />
-                  <div className='play-list-content'>
-                    <div className='play-list-content-title'>Butter</div>
-                    <div className='play-list-content-singer'>
+                  <div className="play-list-content">
+                    <div className="play-list-content-title">Butter</div>
+                    <div className="play-list-content-singer">
                       {/* 방탄소년단(BTS) */}
                     </div>
                   </div>
                 </div>
-                <div className='play-list-right'>
-                  <div className='play-replay-time'>2:45</div>
+                <div className="play-list-right">
+                  <div className="play-replay-time">2:45</div>
                 </div>
               </li>
-              <li className='play-list-li'>
-                <div className='play-list-left'>
+              <li className="play-list-li">
+                <div className="play-list-left">
                   <img
                     src={albumImage}
-                    className='play-list-song-img'
-                    alt='album'
+                    className="play-list-song-img"
+                    alt="album"
                   />
-                  <div className='play-list-content'>
-                    <div className='play-list-content-title'>Butter</div>
-                    <div className='play-list-content-singer'>
+                  <div className="play-list-content">
+                    <div className="play-list-content-title">Butter</div>
+                    <div className="play-list-content-singer">
                       {/* 방탄소년단(BTS) */}
                     </div>
                   </div>
                 </div>
-                <div className='play-list-right'>
-                  <div className='play-replay-time'>2:45</div>
+                <div className="play-list-right">
+                  <div className="play-replay-time">2:45</div>
                 </div>
               </li>
-              <li className='play-list-li'>
-                <div className='play-list-left'>
+              <li className="play-list-li">
+                <div className="play-list-left">
                   <img
                     src={albumImage}
-                    className='play-list-song-img'
-                    alt='album'
+                    className="play-list-song-img"
+                    alt="album"
                   />
-                  <div className='play-list-content'>
-                    <div className='play-list-content-title'>Butter</div>
-                    <div className='play-list-content-singer'>
+                  <div className="play-list-content">
+                    <div className="play-list-content-title">Butter</div>
+                    <div className="play-list-content-singer">
                       {/* 방탄소년단(BTS) */}
                     </div>
                   </div>
                 </div>
-                <div className='play-list-right'>
-                  <div className='play-replay-time'>2:45</div>
+                <div className="play-list-right">
+                  <div className="play-replay-time">2:45</div>
                 </div>
               </li>
-              <li className='play-list-li'>
-                <div className='play-list-left'>
+              <li className="play-list-li">
+                <div className="play-list-left">
                   <img
                     src={albumImage}
-                    className='play-list-song-img'
-                    alt='album'
+                    className="play-list-song-img"
+                    alt="album"
                   />
-                  <div className='play-list-content'>
-                    <div className='play-list-content-title'>Butter</div>
-                    <div className='play-list-content-singer'>
+                  <div className="play-list-content">
+                    <div className="play-list-content-title">Butter</div>
+                    <div className="play-list-content-singer">
                       {/* 방탄소년단(BTS) */}
                     </div>
                   </div>
                 </div>
-                <div className='play-list-right'>
-                  <div className='play-replay-time'>2:45</div>
+                <div className="play-list-right">
+                  <div className="play-replay-time">2:45</div>
                 </div>
               </li>
-              <li className='play-list-li'>
-                <div className='play-list-left'>
+              <li className="play-list-li">
+                <div className="play-list-left">
                   <img
                     src={albumImage}
-                    className='play-list-song-img'
-                    alt='album'
+                    className="play-list-song-img"
+                    alt="album"
                   />
-                  <div className='play-list-content'>
-                    <div className='play-list-content-title'>Butter</div>
-                    <div className='play-list-content-singer'>
+                  <div className="play-list-content">
+                    <div className="play-list-content-title">Butter</div>
+                    <div className="play-list-content-singer">
                       {/* 방탄소년단(BTS) */}
                     </div>
                   </div>
                 </div>
-                <div className='play-list-right'>
-                  <div className='play-replay-time'>2:45</div>
+                <div className="play-list-right">
+                  <div className="play-replay-time">2:45</div>
                 </div>
               </li>
             </ul>
@@ -165,49 +181,64 @@ class MainPlayer extends Component {
           <div className="play-duration">
             <div className="play-duration-bar"></div>
           </div>
+          <div className="player-container">
+            <div id="player"></div>
+            <iframe
+              style={{
+                position: 'relative',
+                top: 30,
+                left: 50,
+                width: '300px',
+                height: '50px',
+              }}
+              src={`https://www.youtube.com/embed/5gq15OVc5wo`}
+              frameBorder="0"
+            />
+            {/* <YouTube videoId="5gq15OVc5wo" opts={options} /> */}
+          </div>
           <div className="play-set-up">
             <div className="play-range">
               <button className="pre-music-btn">
                 <i className="fas fa-step-backward"></i>
               </button>
-              <button className='pause-music-btn'>
-                <i className='fas fa-pause'></i>
+              <button className="pause-music-btn">
+                <i className="fas fa-pause"></i>
               </button>
-              <button className='next-music-btn'>
-                <i className='fas fa-step-forward'></i>
+              <button className="next-music-btn">
+                <i className="fas fa-step-forward"></i>
               </button>
-              <span className='music-time'>2:34/3:37</span>
+              <span className="music-time">2:34/3:37</span>
             </div>
-            <div className='play-song-info'>
-              <div className='play-song-info-content'>
-                <img src={albumImage} className='song-info-img' alt='album' />
-                <div className='song-info-content'>
-                  <div className='song-info-title'>Butter</div>
-                  <div className='song-info-singer'>
+            <div className="play-song-info">
+              <div className="play-song-info-content">
+                <img src={albumImage} className="song-info-img" alt="album" />
+                <div className="song-info-content">
+                  <div className="song-info-title">Butter</div>
+                  <div className="song-info-singer">
                     방탄소년단(BTS) • Butter(Hotter, Sweeter, Cooler) • 2021
                   </div>
                 </div>
-                <div className='song-add'>
+                <div className="song-add">
                   <button
-                    className='song-add-btn'
+                    className="song-add-btn"
                     onClick={() => {
                       this.handleOpenModal();
                     }}
                   >
-                    <i className='fas fa-plus'></i>
+                    <i className="fas fa-plus"></i>
                   </button>
                 </div>
               </div>
             </div>
-            <div className='play-song-setting'>
+            <div className="play-song-setting">
               <button>
-                <i className='fas fa-volume-up'></i>
+                <i className="fas fa-volume-up"></i>
               </button>
               <button>
-                <i className='fas fa-redo'></i>
+                <i className="fas fa-redo"></i>
               </button>
               <button>
-                <i className='fas fa-random'></i>
+                <i className="fas fa-random"></i>
               </button>
             </div>
           </div>
@@ -217,7 +248,6 @@ class MainPlayer extends Component {
           isAddBtnClick={this.state.isAddPlayerBtnClick}
           onCloseAddPlayer={this.handleCloseModal}
         />
-        <div id='player'>{}</div>
       </main>
     );
   }
