0e8758e4e345ba23fd80945918f948ef14dc3cf6
jiye-7 <jy522hi@gmail.com>
2021-06-17 19:56:47 +0900

    Modify MainPlayer

--- client/src/Components/footer/footer.jsx
+++ client/src/Components/footer/footer.jsx
@@ -5,49 +5,49 @@ import logo from '../../Utils/images/main_logo.png';
 class Footer extends Component {
   render() {
     return (
-      <div className='footer-container'>
+      <div className="footer-container">
         <footer>
-          <div className='footer-container-div'>
-            <div className='left-col'>
+          <div className="footer-container-div">
+            <div className="left-col">
               <img
-                className='logo'
+                className="logo"
                 src={logo}
                 style={({ width: 40 }, { height: 40 })}
-                alt='logo'
+                alt="logo"
               />
-              <div className='social-media'>
-                <a href='/'>
-                  <i className='fab fa-facebook-f'></i>
+              <div className="social-media">
+                <a href="/">
+                  <i className="fab fa-facebook-f"></i>
                 </a>
-                <a href='/'>
-                  <i className='fab fa-twitter'></i>
+                <a href="/">
+                  <i className="fab fa-twitter"></i>
                 </a>
-                <a href='/'>
-                  <i className='fab fa-instagram'></i>
+                <a href="/">
+                  <i className="fab fa-instagram"></i>
                 </a>
-                <a href='/'>
-                  <i className='fab fa-youtube'></i>
+                <a href="/">
+                  <i className="fab fa-youtube"></i>
                 </a>
-                <a href='/'>
-                  <i className='fab fa-linkedin-in'></i>
+                <a href="/">
+                  <i className="fab fa-linkedin-in"></i>
                 </a>
               </div>
-              <p className='rights-text'>Copyright © NodongU 2021</p>
+              <p className="rights-text">Copyright © NodongU 2021</p>
             </div>
-            <div className='right-col'>
-              <h1 className='right-col-h1'>Our Newsletter</h1>
-              <div className='border'></div>
+            <div className="right-col">
+              <h1 className="right-col-h1">Our Newsletter</h1>
+              <div className="border"></div>
               <p>Enter Your Email to get our news and updates.</p>
-              <form action='' className='newsletter-form'>
+              <form action="" className="newsletter-form">
                 <input
-                  type='email'
-                  className='txtb'
-                  placeholder='Enter Your Email :)'
+                  type="email"
+                  className="txtb"
+                  placeholder="Enter Your Email :)"
                 />
                 <input
-                  type='submit'
-                  className='newsletter-form-btn'
-                  value='submit'
+                  type="button"
+                  className="newsletter-form-btn"
+                  value="submit"
                 />
               </form>
             </div>
