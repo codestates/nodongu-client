7fe5049b0a29c0384f647cf8bca7c01f5fa324a0
jiye-7 <jy522hi@gmail.com>
2021-06-19 18:53:57 +0900

    Fix editUserInfo Page

--- client/src/Components/editUserInfo/editUserInfo.jsx
+++ client/src/Components/editUserInfo/editUserInfo.jsx
@@ -75,7 +75,6 @@ class EditUserInfo extends PureComponent {
               this.props.updateUserInfo({
                 ...this.props.userInfo,
                 image: reader.result,
-                cd,
               });
             }
           });
@@ -279,10 +278,10 @@ class EditUserInfo extends PureComponent {
     return this.state.isLoading ? (
       <Loading />
     ) : (
-      <div className='edit-container'>
-        <div className='edit-card'>
-          <div className='edit-card__header'>
-            <div className='header__picture'>
+      <div className="edit-container">
+        <div className="edit-card">
+          <div className="edit-card__header">
+            <div className="header__picture">
               <img
                 src={
                   !this.props.userInfo.image
@@ -291,50 +290,50 @@ class EditUserInfo extends PureComponent {
                     ? this.arrayBufferToBase643(this.props.userInfo.image.data)
                     : this.props.userInfo.image
                 }
-                alt='profile'
+                alt="profile"
               />
               <input
                 ref={this.imageValueRef}
-                type='file'
-                name='image-upload'
-                id='input'
-                accept='image/*'
+                type="file"
+                name="image-upload"
+                id="input"
+                accept="image/*"
                 onChange={this.imageHandler}
               />
-              <label htmlFor='input' className='image-upload'>
+              <label htmlFor="input" className="image-upload">
                 이미지 업로드
               </label>
-              <button type='button' onClick={this.handleDeleteImage}>
+              <button type="button" onClick={this.handleDeleteImage}>
                 이미지 제거
               </button>
             </div>
-            <div className='header__divided-line'></div>
-            <div className='header__nickname'>
-              <h2 className='nickname__nickname'>
+            <div className="header__divided-line"></div>
+            <div className="header__nickname">
+              <h2 className="nickname__nickname">
                 <span
                   ref={this.nicknameRef}
                 >{`${this.props.userInfo.nickname}님, 어서오세요!`}</span>
                 <input
                   ref={this.nicknameInput}
-                  className='nickname__input hidden'
-                  type='text'
-                  placeholder='변경할 닉네임'
+                  className="nickname__input hidden"
+                  type="text"
+                  placeholder="변경할 닉네임"
                   onChange={this.onExistNickNameCheck}
                 />
               </h2>
-              <div className='nickname__button-container'>
+              <div className="nickname__button-container">
                 <button
                   ref={this.nicknameButtonRef}
-                  className='nickname__button nickname__button--edit'
-                  type='button'
+                  className="nickname__button nickname__button--edit"
+                  type="button"
                   onClick={this.handleEditName}
                 >
                   수정
                 </button>
                 <button
                   ref={this.nicknameEditButton}
-                  className='nickname__button nickname__button--submit hidden'
-                  type='button'
+                  className="nickname__button nickname__button--submit hidden"
+                  type="button"
                   onClick={this.handleEditNicknameChange}
                 >
                   변경
@@ -342,36 +341,36 @@ class EditUserInfo extends PureComponent {
               </div>
             </div>
           </div>
-          <div className='edit-card__body'>
-            <ul className='edit-card__info'>
-              <li className='info__email'>
+          <div className="edit-card__body">
+            <ul className="edit-card__info">
+              <li className="info__email">
                 <div>
                   <h2>이메일</h2>
                   <span>{this.props.userInfo.email}</span>
                 </div>
                 <hr />
               </li>
-              <li className='info__password'>
+              <li className="info__password">
                 <div>
                   <h2>패스워드</h2>
-                  <span className=''>
+                  <span className="">
                     {this.state.userInfo.password
                       .split('')
                       .map((el) => '*')
                       .join('')}
                   </span>
                   <input
-                    type='password'
-                    className='info__password__input hidden'
-                    placeholder='변경할 비밀번호'
+                    type="password"
+                    className="info__password__input hidden"
+                    placeholder="변경할 비밀번호"
                     onChange={this.onPasswordCheck}
                   />
-                  <button type='button' onClick={this.handleEditPassword}>
+                  <button type="button" onClick={this.handleEditPassword}>
                     수정
                   </button>
                   <button
-                    className='hidden'
-                    type='button'
+                    className="hidden"
+                    type="button"
                     onClick={this.handleEditPasswordRequest}
                   >
                     변경
@@ -379,11 +378,11 @@ class EditUserInfo extends PureComponent {
                 </div>
                 <hr />
               </li>
-              <li className='info__deleteUser'>
+              <li className="info__deleteUser">
                 <div>
                   <h2>회원 탈퇴</h2>
                 </div>
-                <button type='button' onClick={this.handleDeleteUser}>
+                <button type="button" onClick={this.handleDeleteUser}>
                   회원 탈퇴
                 </button>
               </li>
